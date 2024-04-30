e56de3eb836366ba9edd4dc7d3e32f17e55a5a12
jennydunix <jennifer.o.odunze@gmail.com>
2024-05-01 00:58:11 +0100

    completed styling of page layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -9,10 +9,10 @@ const DashboardLayout = ({
     return ( 
         <div className="h-full">
             <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
-                <Navbar />
+                <Navbar/>
             </div>
             {/* sidebar */}
-            <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-56">
+            <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
                 <Sidebar />
             </div>
             <main className="md:pl-56 h-full">{children}</main>

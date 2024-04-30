d1318e45a3dae95d3469655974ae0eee4558c093
jennydunix <jennifer.o.odunze@gmail.com>
2024-04-30 23:43:18 +0100

    created page layout

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export const Logo = () => {
+    return (
+        <Image
+        src="/ha-muta.svg"
+        height={100} width={100} alt="logo" />
+    )
+}
---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,5 @@
+export const Navbar = () => {
+    return (
+        <div className="p-4 border-b h-full flex items-centerbg-white shadow-sm"></div>
+    )
+}

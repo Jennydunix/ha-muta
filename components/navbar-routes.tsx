9e2efc64aa37994dbf6419f213ead64595ab38b8
jennydunix <jennifer.o.odunze@gmail.com>
2024-05-02 21:11:56 +0100

    working on dynamic layout

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,10 +1,31 @@
 "use client"
 
 import { UserButton } from "@clerk/nextjs"
+import { usePathname, useRouter } from "next/navigation"
+import { Button } from "@/components/ui/button";
+
+import { Link, LogOut } from "lucide-react";
 
 export const NavbarRoutes = () => {
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const isTeacherPage = pathname?.startsWith("/teacher");
+    const isPlayerPage = pathname?.includes("/chapter");
     return (
-        <div className="flex gap-x-2 ml-auto ">
+        <div className="flex gap-x-2 ml-auto">
+            {isTeacherPage || isPlayerPage ? (
+                <Button>
+                    <LogOut className="h-4 w-4 mr-2" />
+                    Exit
+                </Button>
+            ) : (
+                <Link href="/teacher/courses ">
+                    <Button>
+                        Teacher mode
+                    </Button>
+                </Link>
+            )}
             <UserButton />
         </div>
     )
